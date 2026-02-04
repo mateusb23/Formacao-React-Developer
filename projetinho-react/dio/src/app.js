fcbaf2f3e499881a3ac014c6a468836ad347de94
mateusb23 <mateusb.bispo@gmail.com>
2026-02-04 00:29:46 -0300

    atualização sobre o ciclo de vida de components

--- projetinho-react/dio/src/app.js
+++ projetinho-react/dio/src/app.js
@@ -1,7 +1,34 @@
+import react, { useEffect, useState } from "react";
+
 const App = () => {
+  const [usuario, setUsuario] = useState("");
+  const [count, setCount] = useState(0);
+  const [usuarios, setUsuarios] = useState(["Pablo","José","Manoel"]);
+  
+  const handleAddUser = () => {
+    setUsuarios([...usuarios, usuario]);
+    setUsuario("");
+  };
+
+  useEffect(() => {
+    setCount(usuarios.length);
+  }, [usuarios]);
+
   return (
     <div className="App">
         <h1>Hello, DIO!</h1>
+        <h3>Total: {count}</h3>
+        <div>
+          <input
+            value={usuario}
+            onChange={(event) => setUsuario(event.target.value)}
+          />
+            <button onClick={handleAddUser}>Adicionar</button>
+        </div>
+        <hr />
+        {usuarios.map((item) => (
+          <p>{item}</p>
+        ))}
     </div>
   );
 };
