493286238b513ce1fa4b2a15a885e0401faca6c0
mateusb23 <mateusb.bispo@gmail.com>
2026-02-06 18:00:03 -0300

    Atualização do styles.css global

--- App_GitFind/gitfind/src/index.js
+++ App_GitFind/gitfind/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles.css';
 import App from './src/pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
