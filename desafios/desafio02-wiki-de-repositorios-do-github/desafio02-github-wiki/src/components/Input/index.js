a5d40f31c46f25f6ec54d1e963dc432dc7717e17
mateusb23 <mateusb.bispo@gmail.com>
2026-02-19 23:58:39 -0300

    criando pages e components

---
+++ desafios/desafio02-wiki-de-repositorios-do-github/desafio02-github-wiki/src/components/Input/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { InputContainer } from './styles'
+
+function Input() {
+  return (
+    <div>
+        <InputContainer><input /></InputContainer>
+    </div>
+  )
+}
+
+export default Input
