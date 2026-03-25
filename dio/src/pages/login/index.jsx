5d2dc77779439dd330c0ae470d3f8956c3b453d2
mateusb23 <mateusb.bispo@gmail.com>
2026-03-25 00:33:14 -0300

    atualizando o component Input e a page Login

--- dio/src/pages/login/index.jsx
+++ dio/src/pages/login/index.jsx
@@ -1,11 +1,24 @@
-import { Link } from "react-router-dom"
+import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
+import { Input } from "../../components/Input"
+
+import { Container, Title } from './styles';
 
 const Login = () => {
   return (<>
-    <h1>Login</h1>
-    <Link to="/">
-      <button>Voltar para home</button>
-    </Link>
+    <Header />
+    <Container>
+      <div>
+        <Title> 
+          
+          O seu futuro global agora!
+        </Title>
+        <Button title="Começar agora" variant="secondary" onClick={() => null}/>
+      </div>
+      <div>
+        <Input placeholder="E-mail" />
+      </div>
+    </Container>
   </>)
 }
 
